311174ec19ed394700dcff8443235a5c24da36fe
Szymon Jablonski <jablonskiszymon04@gmail.com>
2025-11-23 17:17:22 +0100

    Changed the values in txt files and the python script.

--- python_scripts/task1-plot.py
+++ python_scripts/task1-plot.py
@@ -4,27 +4,26 @@ import matplotlib.pyplot as plt
 from scipy.fft import fft, fftfreq
 
 def plot_frequency_spectrum(signal, fs, title):
-    # Needed to remove the DC offset
+    # Need to remove the DC offset
     signal = np.array(signal) - np.mean(signal)
     N = len(signal)
-     
+    
+    # Apply windowing function
     window = np.hanning(N)
     signal = signal * window
   
+    # Perform FFT
     fft_vals = fft(signal)
     fft_freq = fftfreq(N, 1/fs)# Plot magnitude spectrum (positive frequencies only)
     
     # Single-sided spectrum
     mag = np.abs(fft_vals[:N//2])
 
-    # Normalize (optional but recommended)
-
     # Convert to dB (add small value to avoid log(0))
     mag_db = 20 * np.log10(mag + 1e-12)
 
     plt.figure(figsize=(10, 5))
     # :N//2 - return only first half - the positive frequencies
-    # 2/N * np.abs(...) - normalize the amplitude, factor of 2 for single-sided spectrum
     plt.plot(fft_freq[:N//2], mag_db)
     plt.title(title)
     plt.xlabel('Frequency (Hz)')
@@ -44,6 +43,7 @@ def process_adc_vals(filenames, sampling_rate):
                 if match:
                     adc_values.append(int(match.group(1)))
         
+        # Create time array
         t = np.arange(len(adc_values)) / sampling_rate
         
         plt.figure(figsize=(10, 5))
@@ -56,5 +56,5 @@ def process_adc_vals(filenames, sampling_rate):
 
         plot_frequency_spectrum(adc_values, sampling_rate, "FFT for signal from " + filename)
         
-files = ["1khz.txt", "10khz.txt", "40khz.txt", "1khz-t2.txt", "10khz-t2.txt", "my-1khz-t2.txt", "my-10khz-t2.txt", "my-40khz-t2.txt"]
+files = ["1khz.txt", "10khz.txt", "40khz.txt", "my-1khz-t2.txt", "my-10khz-t2.txt", "my-40khz-t2.txt"]
 process_adc_vals(files, 50000)
